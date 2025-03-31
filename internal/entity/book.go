a511bbc5c77fc488687d25533a27a3235f0d4a41
a.nurlan <a.nurlan@documentolog.com>
2025-03-31 15:41:49 +0500

    DTO

--- internal/entity/book.go
+++ internal/entity/book.go
@@ -3,8 +3,17 @@ package entity
 import "time"
 
 type Book struct {
-	ID        int64
-	Title     string
-	Author    string
-	Published time.Time
+	ID          int64     `json:"id"`
+	Title       string    `json:"title"`
+	Author      string    `json:"author"`
+	Publisher   string    `json:"publisher"`
+	ISBN        string    `json:"isbn"`
+	Genre       string    `json:"genre"`
+	Stock       int       `json:"stock"`
+	TotalPages  int       `json:"total_pages"`
+	Language    string    `json:"language"`
+	PublishedAt time.Time `json:"published_at"`
+	Description string    `json:"description"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at"`
 }
